8efefe9f5074395784bf95e08001b67063077950
Kenton Cargile <49448362+KentonCargile@users.noreply.github.com>
2020-11-25 19:31:39 +0100

    mvp

--- index.js
+++ index.js
@@ -1,74 +1,148 @@
+var cvs = document.getElementById("canvas");
+
+var ctx = cvs.getContext("2d");
+
+// load images
+
+var fish = new Image();
+
+var background = new Image();
+
+var coral = new Image();
+
+var topObstacle = new Image();
+
+var bottomObstacle = new Image();
+
+fish.src = "images/fish.png";
+
+background.src = "images/background.png";
+
+coral.src = "images/coral.png";
+
+topObstacle.src = "images/topObstacle.png";
+
+bottomObstacle.src = "images/bottomObstacle.png";
 
 
+// some variables
 
-var player = {
-    x: {150, y: 250, size: 50}
-    var gravity = 0;
-    var goUp = false;
-    var crashed = false;
+var gap = 85;
+
+var constant;
+
+var bX = 10;
+
+var bY = 150;
+
+var gravity = 1.5;
+
+var score = 0;
+
+// audio files
+
+var fly = new Audio();
+
+var scor = new Audio();
+
+fly.src = "sounds/fly.mp3";
+
+scor.src = "sounds/score.mp3";
+
+// on key down
+
+document.addEventListener("keydown",moveUp);
+
+function moveUp(){
+
+    bY -= 25;
+
+    fly.play();
 
 }
 
-var draw = function () {
-    background(0,0,0);
-    drawPlayer();
-    movePlayer();
+// pipe coordinates
 
-    if (crashed === false) {
-        movePlayer();
-    } else {
-        youLoseScreen();
-    }
+var obstacle = [];
+
+obstacle[0] = {
+
+    x : cvs.width,
+
+    y : 0
+
 };
 
-var drawPlayer = function () {
-    fill (0, 0, 225);
-    ellipse (player.x, player.y, player.size, player.size, player.size)
+// draw images
+
+function draw(){
+
+  
+    ctx.drawImage(background,0,0);
+
+    for(var i = 0; i < obstacle.length; i++){     
+
+        constant = topObstacle.height+gap;
+
+        ctx.drawImage(topObstacle,obstacle[i].x,obstacle[i].y);
+
+        ctx.drawImage(bottomObstacle,obstacle[i].x,obstacle[i].y+constant);           
+
+        obstacle[i].x--;      
+
+        if( obstacle[i].x == 125 ){
+
+            obstacle.push({
+
+                x : cvs.width,
+
+                y : Math.floor(Math.random()*topObstacle.height)-topObstacle.height
+
+            });
+
+        }
+
+        // detect collision
+      
+
+        if( bX + fish.width >= obstacle[i].x && bX <= obstacle[i].x + topObstacle.width && (bY <= obstacle[i].y + topObstacle.height || bY+fish.height >= obstacle[i].y+constant) || bY + fish.height >=  cvs.height - coral.height){
+
+            location.reload(); // reload the page
+
+        }
+
+     
+        if(obstacle[i].x == 5){
+
+            score++;
+
+            scor.play();
+
+        }
+          
+
+    }
+
+    ctx.drawImage(coral,0,cvs.height - coral.height);
+
+  
+
+    ctx.drawImage(fish,bX,bY);
+ 
+
+    bY += gravity;
+
+  
+
+    ctx.fillStyle = "#000";
+
+    ctx.font = "20px Verdana";
+
+    ctx.fillText("Score : "+score,10,cvs.height-20);
+
+  
+    requestAnimationFrame(draw);  
+
 }
 
-var movePlayer = function () {
-    if (goUp) {
-        gravity -= 0.4;
-    } else {
-        gravity += 0.4;
-    }
-       
-    player.y += gravity;
-
-    if player.y > 500 || player.y < 0 {
-        crashed = true;
-    }
-};
-
-
-var mousePressed = function () {
-    if (mouseButton === LEFT ){
-    //if the left mouse button has been pressed
-        goUp = true;
-    // then if the player has crashed
-    if (crashed){
-    // set crashed back to false
-    // set the player's y position back to the center 
-    // set gravity back to 0    
-
-
-    }
-    
-    }
-}
-
-var mouseReleased = function () {
-    if (mouseButton === LEFT ){
-        goUp = false;
-    }
-}
-
-console.log(goUp);
-
-
-var youLoseScreen = function () {
-    fill(255);
-    textSize(24);
-    text("Game Over", 200, 200);
-    text("Click to Restart", 180, 350);
-};
+draw();
